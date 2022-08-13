f809286475a04ff829724e19629e2adbfdeca755
guvin-vintage <109299594+guvin-vintage@users.noreply.github.com>
2022-08-13 16:22:36 +0700

    aa

--- news.js
+++ news.js
@@ -168,6 +168,19 @@ $(document).ready(function () {
         })
     })
 
+    
+
+    wow = new WOW(
+        {
+        boxClass:     'wow',      // default
+        animateClass: 'animate__animated', // default
+        offset:       0,          // default
+        mobile:       true,       // default
+        live:         true        // default
+      }
+      )
+      wow.init();
+
 
 
 })
