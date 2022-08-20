4da08e09e9b1c9727b82ac90448172b5ae921e26
guvin-vintage <109299594+guvin-vintage@users.noreply.github.com>
2022-08-21 00:39:50 +0700

    a

--- news.js
+++ news.js
@@ -107,7 +107,10 @@ function checkMail() {
 }
 
 
-
+$(window).on('load', function (event) {
+    $('body').removeClass('preloading');
+    $('.load').delay(1000).fadeOut('fast');
+})
 
 $(document).ready(function () {
     $("#gototop").hide();
